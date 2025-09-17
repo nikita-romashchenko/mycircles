3420882dddf644b73201fcb36a20900b5702456c
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2025-09-17 14:26:03 +0300

    feat: add Tailwind CSS and related plugins
    - Updated package.json to include Tailwind CSS and its plugins.
    - Added app.css to import Tailwind CSS and the forms plugin.
    - Modified +layout.svelte to import app.css for styling.
    - Updated vite.config.js to include Tailwind CSS as a plugin.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import Header from "$components/header.svelte"
-  import Footer from "$components/footer.svelte"
-
+  import "../app.css";
+  import Header from "$components/header.svelte";
+  import Footer from "$components/footer.svelte";
+  
   export let data;
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [sveltekit()],
-})
+  server: { port: 3000 },
+  plugins: [tailwindcss(), sveltekit()]
+});
